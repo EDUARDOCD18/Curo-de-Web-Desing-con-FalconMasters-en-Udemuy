51bba08a056a5a5fb51c25db19e16c54caffebbe
eduardocd18 <javierecd@gmail.com>
2026-01-20 05:00:50 -0400

    141. Importando imágenes

--- VITE/src/main.js
+++ VITE/src/main.js
@@ -1,1 +1,8 @@
-document.querySelector("#app").innerHTML = `<h1>Hola, mundo</h1>`;
+import desierto from "/assets/desierto.png"
+
+document.querySelector("#app").innerHTML = `
+<h1>Hola, mundo</h1>
+
+<img src="${desierto}" "alt="Imagen de desierto">
+
+`;
