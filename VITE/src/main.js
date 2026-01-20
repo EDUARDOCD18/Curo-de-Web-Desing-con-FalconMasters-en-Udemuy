eb512177fcfb56efbae68f197497805cbdfcc4d6
eduardocd18 <javierecd@gmail.com>
2026-01-20 05:15:16 -0400

    142. Módulos de ES6

--- VITE/src/main.js
+++ VITE/src/main.js
@@ -1,8 +1,12 @@
-import desierto from "/assets/desierto.png"
+import desierto from "/assets/desierto.png";
+import cargarContador from "./contador";
 
 document.querySelector("#app").innerHTML = `
 <h1>Hola, mundo</h1>
 
-<img src="${desierto}" "alt="Imagen de desierto">
+<img src="${desierto}" "alt="Imagen de desierto" id="imagen">
+<h2>Veces cliqueado: <span id="cuenta">0</span></2>
 
 `;
+
+cargarContador();
