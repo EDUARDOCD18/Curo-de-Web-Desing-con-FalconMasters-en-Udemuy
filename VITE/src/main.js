bef4f0184528c160e8efcf3c6f180822969fe8ff
eduardocd18 <javierecd@gmail.com>
2026-01-22 20:19:58 -0400

    143. Importando archivos de CSS

--- VITE/src/main.js
+++ VITE/src/main.js
@@ -1,11 +1,13 @@
 import desierto from "/assets/desierto.png";
 import cargarContador from "./contador";
+import "../estilos.css";
+import clases from "../imagen.module.css";
 
 document.querySelector("#app").innerHTML = `
 <h1>Hola, mundo</h1>
 
-<img src="${desierto}" "alt="Imagen de desierto" id="imagen">
-<h2>Veces cliqueado: <span id="cuenta">0</span></2>
+<img src="${desierto}" "alt="Imagen de desierto" id="imagen" class="${clases.imagen}">
+<h2>Veces cliqueado: <span id="cuenta" class="${clases.contador}">0</span></2>
 
 `;
 
