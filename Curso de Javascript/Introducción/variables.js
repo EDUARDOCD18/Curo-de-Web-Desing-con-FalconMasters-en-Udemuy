efb5002f71cc29cf5e116f20c65981c1bf9a73a8
eduardocd18 <javierecd@gmail.com>
2026-02-04 05:22:54 -0400

    170. Variables

---
+++ Curso de Javascript/Introducción/variables.js
@@ -0,0 +1,35 @@
+var edad = 26;
+console.log("Mi edad es de", edad);
+
+let nombre = "Javier Eduardo";
+console.log("Mi nombre es", nombre);
+
+const correo = "correo@correo.com";
+console.log("El correo es", correo);
+
+nombre = "Javi";
+console.log("Mi nombre es", nombre);
+
+let telefono, pais, id;
+
+telefono = "1234567890";
+console.log(telefono);
+
+pais = "Francia";
+console.log(pais);
+
+id = 2;
+console.log(id);
+
+const resultado = 4 + 4;
+console.log(resultado);
+
+const nombre1 = "Carlos",
+  nombre2 = " Javier",
+  nombreCompleto = nombre1 + nombre2;
+console.log(nombreCompleto);
+
+let miVariable = "Hola"
+console.log(miVariable);
+miVariable = 2
+console.log(miVariable);
