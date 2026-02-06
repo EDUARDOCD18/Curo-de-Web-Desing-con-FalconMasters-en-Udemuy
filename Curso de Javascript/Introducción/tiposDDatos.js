80fbf80115e0d1b1f414f3aaa081e3f1c9baf61b
eduardocd18 <javierecd@gmail.com>
2026-02-06 05:06:53 -0400

    172. Tipos de datos (parte 2)

--- Curso de Javascript/Introducción/tiposDDatos.js
+++ Curso de Javascript/Introducción/tiposDDatos.js
@@ -8,7 +8,32 @@ const numero2 = 5;
 console.log(numero + numero2);
 
 /* Boolean */
-const usuarioConectado = false
+const usuarioConectado = false;
 console.log(usuarioConectado);
-const mayorQue = 1<2
+const mayorQue = 1 < 2;
 console.log(mayorQue);
+
+/* Arrays */
+const array = [1, 2, 5, "Javier", { propiedad: "Eduardo" }, [2, 2, 2]];
+console.log(array);
+
+/* Objeto */
+const persona = {
+  nombre: "Javier",
+  edad: 26,
+  numero: 101235813,
+  carro: {
+    marca: "...",
+    color: "blanco",
+  },
+};
+
+console.log(persona);
+console.log(persona.carro.color);
+
+/* Función */
+function hola() {
+  console.log("Hola");
+}
+
+hola();
