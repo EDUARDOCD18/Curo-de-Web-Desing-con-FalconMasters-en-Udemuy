82e3226f885d771890e9ee7dd6c3df6902dc675c
eduardocd18 <javierecd@gmail.com>
2026-02-06 04:58:11 -0400

    171. Tipos de datos (parte 1)

---
+++ Curso de Javascript/Introducción/tiposDDatos.js
@@ -0,0 +1,14 @@
+const nombre = "Javier";
+const parrafo = 'Hola mundo en "párrafo"';
+console.log(parrafo);
+
+/* Números */
+const numero = 3;
+const numero2 = 5;
+console.log(numero + numero2);
+
+/* Boolean */
+const usuarioConectado = false
+console.log(usuarioConectado);
+const mayorQue = 1<2
+console.log(mayorQue);
