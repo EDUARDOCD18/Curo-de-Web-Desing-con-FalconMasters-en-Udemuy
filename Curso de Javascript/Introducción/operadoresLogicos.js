d47ef91a1724a721ec89b85bfaba6b3f5f27b936
eduardocd18 <javierecd@gmail.com>
2026-02-07 08:19:31 -0400

    174. Operadores lógicos

---
+++ Curso de Javascript/Introducción/operadoresLogicos.js
@@ -0,0 +1,19 @@
+const nombre = "Javier";
+const edad = 26;
+const tieneEntrada = true;
+const tienePermido = true;
+
+/* -- Operador And --*/
+/* const acceso = edad > 18 && tieneEntrada;
+console.log("Acceso permitido al concierto: " + acceso);
+ */
+
+/* -- Operador Or --*/
+/* const permitirAcceso =
+  (edad >= 18 && tieneEntrada) || (tienePermido && tieneEntrada);
+console.log("Acceso permitido al concierto: " + permitirAcceso); */
+
+/* -- Operador Not -- */
+const variable = true
+console.log(variable);
+console.log(!variable);
