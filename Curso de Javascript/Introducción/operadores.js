d7b04dedce08bf4eb845382435bd81f31f2bfb4b
eduardocd18 <javierecd@gmail.com>
2026-02-07 07:58:31 -0400

    173. Operadores aritméticos y de comparación

---
+++ Curso de Javascript/Introducción/operadores.js
@@ -0,0 +1,48 @@
+/* Operador de suma */
+const suma = 10 + 10;
+console.log(suma);
+
+/* Operador de resta */
+const resta = 10 - 3;
+console.log(resta);
+
+/* Operador de módulo, el restante de la división */
+const modulo = 10 % 3;
+console.log(modulo);
+
+/* Aumento y disminución */
+let numero = 1;
+console.log(numero);
+numero++;
+console.log(numero);
+numero--;
+console.log(numero);
+
+/* Asignación */
+let numero2 = 10;
+numero2 += 5;
+console.log(numero2);
+numero2 -= 2;
+console.log(numero2);
+
+/* Operadores de comparación */
+/* const resultado = 5 > 1
+const resultado2 = 10 < 100
+const resultado3 = 10 > 100
+const resultado4 = 10 == "10"
+const resultado5 = 10 === 10
+console.log(resultado);
+console.log(resultado2);
+console.log(resultado3);
+console.log(resultado4);
+console.log(resultado5);
+ */
+
+/* Operador Ternario */
+console.log("Operador ternario");
+const resultado =
+  7 > 2
+    ? "El primer valor es mayor que el segundo"
+    : "El segundo valor es mayor que el primero";
+
+console.log(resultado);
