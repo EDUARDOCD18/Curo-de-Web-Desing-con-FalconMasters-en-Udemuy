1a85a1243c1b6524e67a84670e4ddf5551923427
eduardocd18 <javierecd@gmail.com>
2026-02-09 07:07:07 -0400

    177. Template Strings

---
+++ Curso de Javascript/Introducción/templateStrings.js
@@ -0,0 +1,7 @@
+const nombre = "Javier Eduado";
+const edad = "26";
+const pais = "Venezuela";
+
+const mensaje = `La persona es ${nombre}, tiene ${edad} años y es de ${pais}.`
+
+console.log(mensaje);
