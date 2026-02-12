bce264b9f18f4ce376d1950e10213be07fdc9e14
eduardocd18 <javierecd@gmail.com>
2026-02-12 05:41:25 -0400

    180. Funciones

---
+++ Curso de Javascript/Introducción/funciones.js
@@ -0,0 +1,18 @@
+// console.log("Hola, mundo.");
+
+/* Forma 1 */
+/* function saludo() {
+  console.log("Hola, mundo desde función en forma 1.");
+}
+ */
+/* Forma 2 */
+const saludo = function () {
+  console.log("Hola mundo desde función en forma 2.");
+};
+
+saludo();
+
+/* Forma 3, arrow functions */
+const forma3 = () => console.log("Hola desde arrow function");
+
+forma3();
