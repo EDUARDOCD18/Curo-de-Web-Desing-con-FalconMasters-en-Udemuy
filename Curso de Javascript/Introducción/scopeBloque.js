4db4d4764352722031163d7fe4cb974dfc5a1662
eduardocd18 <javierecd@gmail.com>
2026-02-15 16:05:43 -0400

    185. Variables de bloque

---
+++ Curso de Javascript/Introducción/scopeBloque.js
@@ -0,0 +1,27 @@
+/* const edad = 19;
+
+if (edad >= 18) {
+  const accesoPermitido = true;
+  console.log(accesoPermitido, "1");
+
+  if (true) {
+    console.log(accesoPermitido, "3");
+  }
+
+  const miFuncion = () => {
+    console.log(accesoPermitido, "4");
+  };
+
+  miFuncion();
+}
+
+const accesoPermitido = "Sí";
+console.log(accesoPermitido, "2");
+ */
+
+if (true) {
+  var nombre = "Eduardo";
+  
+}
+
+console.log(nombre);
