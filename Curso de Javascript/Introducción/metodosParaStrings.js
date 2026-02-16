d6804fa5c8ea5d6fafbffcbeb3174a07da8f7795
eduardocd18 <javierecd@gmail.com>
2026-02-16 15:23:46 -0400

    191. Métodos para trabajar con cadenas de texto

---
+++ Curso de Javascript/Introducción/metodosParaStrings.js
@@ -0,0 +1,26 @@
+/* .legth() */
+/* const texto = "Hola, wapo. Ingeniero";
+console.log(texto.length);
+ */
+
+/* .indexOf() - lastIndexOf() */
+/* const texto = "Hola, wapo. Ingeniero";
+console.log(texto.indexOf("o"));
+console.log(texto.lastIndexOf(".")); */
+
+/* .slice() */
+// const texto = "Hola, wapo. Ingeniero";
+// const index = texto.indexOf(",");
+// const lastIndex = texto.lastIndexOf("g");
+// console.log(index);
+// console.log(lastIndex);
+// console.log(texto.slice(-6, -1));
+
+/* .replace() */
+// const texto = "Hola, wapo. Ingeniero";
+// console.log(texto.replace("Hola", "Epa"));
+
+/* -- .split() --  */
+const texto = "Hola, wapo. Ingeniero";
+cadena = texto.split(" ")
+console.log(cadena);
