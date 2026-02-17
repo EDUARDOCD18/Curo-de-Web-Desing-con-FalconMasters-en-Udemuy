30a47680550f11e9b475e1df57fe56aea58c7869
eduardocd18 <javierecd@gmail.com>
2026-02-17 14:42:40 -0400

    194. Ciclo for

---
+++ Curso de Javascript/Introducción/cicloFor.js
@@ -0,0 +1,12 @@
+const frutas = ["Banana", "Kiwi", "Manzana", "Fresa", "Melón", "Pitalla", "Durazno"];
+/* frutas.forEach((frutas) => {
+  console.log(frutas);
+}); */
+
+for (let numero = 1; numero < 11; numero++) {
+  console.log(numero);
+}
+
+for(let numero = 0; numero < frutas.length; numero++){
+    console.log(numero, frutas[numero]);
+}
