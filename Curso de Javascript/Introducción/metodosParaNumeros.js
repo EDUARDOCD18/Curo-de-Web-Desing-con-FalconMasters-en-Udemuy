b2ac1d6399b20af560bc3e63a04df8840465a64a
eduardocd18 <javierecd@gmail.com>
2026-02-17 12:53:59 -0400

    192. Métodos para trabajar con números

---
+++ Curso de Javascript/Introducción/metodosParaNumeros.js
@@ -0,0 +1,43 @@
+/* .toString */
+// const numero = 10;
+// console.log(numero, typeof numero);
+
+// const texto = numero.toString()
+// console.log(texto, typeof texto);
+
+/* .toFixed() */
+// const numero =  3.1416
+// console.log(numero);
+// console.log(numero.toFixed(1));
+// console.log(numero.toFixed(2));
+// console.log(numero.toFixed(3));
+// console.log(numero.toFixed(10));
+
+/* parseInt() */
+// const numero1 = parseInt(prompt("Escribe un número"));
+// const numero2 = parseInt(prompt("Escribe un segumndo número"));
+// console.log(numero1 + numero2);
+
+/* parseFloat() */
+/* const numero1 = parseFloat(prompt("Escribe un número"));
+const numero2 = parseFloat(prompt("Escribe un segumndo número"));
+console.log(numero1 + numero2);
+ */
+
+/*  Math.random() */
+// const numero = Math.random()
+// console.log(numero);
+
+/* .Math.floor() */
+// console.log(Math.floor(10.6));
+
+/* .Math.ceil() */
+// console.log(Math.ceil(10.6));
+
+/* .Math.round() */
+// console.log(Math.round(100.1));
+
+/* Número aleatorio entre 0 y 100 */
+const numero = Math.random();
+console.log(numero);
+console.log(Math.floor(numero * 101));
