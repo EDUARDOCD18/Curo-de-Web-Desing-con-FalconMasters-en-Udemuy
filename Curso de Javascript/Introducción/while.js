ee707ad12f1b114c4b34ed59f3706c482ab9c2dd
eduardocd18 <javierecd@gmail.com>
2026-02-18 05:05:59 -0400

    196. Ciclo while

---
+++ Curso de Javascript/Introducción/while.js
@@ -0,0 +1,11 @@
+let contador = 0;
+while (contador <= 10) {
+  console.log(contador);
+  contador++;
+}
+
+/* -- do while -- */
+do {
+  console.log(contador);
+  contador++;
+} while (contador <= 20);
