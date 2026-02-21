0989087d2e4d409ccee182f46cc97f357f0647f3
eduardocd18 <javierecd@gmail.com>
2026-02-21 07:53:18 -0400

    200. Seleccionando elementos del DOM (parte 1)

---
+++ Curso de Javascript/DOM/obtenerElementos.js
@@ -0,0 +1,20 @@
+/* getElementByUd */
+const elemento = document.getElementById("contenedor1")
+// console.log(elemento);
+
+/* .children */
+// console.log(elemento.children);
+
+/* parentElement */
+// console.log(elemento.parentElement);
+
+/* getElementByTagName */
+
+// const etiqueta = document.getElementsByTagName("div")
+// console.log(etiqueta);
+// console.log(etiqueta.length);
+
+/* -- getElementByClassName -- */
+
+const etiqueta = document.getElementsByClassName("caja")
+console.log(etiqueta);
