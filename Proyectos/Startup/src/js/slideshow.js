c9c7d98f12b40f021f0d425b3c8dd3342037ff63
eduardocd18 <javierecd@gmail.com>
2026-03-01 19:31:12 -0400

    213. Agregando los indicadores al slideshow

--- Proyectos/Startup/src/js/slideshow.js
+++ Proyectos/Startup/src/js/slideshow.js
@@ -2,6 +2,9 @@ const slideshow = document.getElementById("slideshow");
 const slides = document.getElementById("slides");
 const indicadores = document.getElementById("indicadores");
 const btnCambiar = document.getElementById("cambiar");
+const velocidadDeCambio = 4000;
+
+let slideActual = 1;
 
 /* -- Función para el cambio del slider --  */
 const siguienteSlide = () => {
@@ -19,8 +22,20 @@ const siguienteSlide = () => {
     slides.style.transition = "none";
     slides.style.transform = "translateX(0)";
   }, velocidad);
+
+  // Cambiar los indicadores de los slides
+  if (slideActual < slides.children.length) {
+    slideActual++;
+    indicadores.children[slideActual - 1].classList.add("active");
+  } else {
+    slideActual = 1;
+  }
+
+  indicadores.querySelector(".active").classList.remove("active");
+  indicadores.children[slideActual - 1].classList.add("active");
 };
 
+/* -- Llamar a la función que cambia los slides cada x segundos */
 setInterval(() => {
   siguienteSlide();
-}, 5000);
+}, velocidadDeCambio);
