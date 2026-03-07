caf34546e098af82018a7b63ead2ed62924259d1
eduardocd18 <javierecd@gmail.com>
2026-03-07 17:59:33 -0400

    218. Introducción a JSX

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,5 +1,14 @@
 function App() {
+  const nombre = "Javier Eduardo";
 
+  return (
+    <div className="contenedor">
+      <header className="header">
+        <h1>Hola mundo, soy {nombre}</h1>
+        <p> Hoy es {new Date().toLocaleDateString()}</p>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
